ba21a4b7f05331805b0a068de3891467daa605e9
Popovichenko Oksana <popovichenko.oksana@gmail.com>
2020-10-20 18:50:34 +0300

    Updates

---
+++ precache-manifest.30dba45970cb57c603d2a753396e27d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1724f84fa6f0d5a472513312a38458f1",
+    "url": "/ColorPicker/index.html"
+  },
+  {
+    "revision": "9a7fcfe36e505a1d89bc",
+    "url": "/ColorPicker/static/css/main.eca98a33.chunk.css"
+  },
+  {
+    "revision": "57548a4ebf49f4929b9c",
+    "url": "/ColorPicker/static/js/2.b0fbae68.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ColorPicker/static/js/2.b0fbae68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7fcfe36e505a1d89bc",
+    "url": "/ColorPicker/static/js/main.effbd298.chunk.js"
+  },
+  {
+    "revision": "9734cbae675ccaf31346",
+    "url": "/ColorPicker/static/js/runtime-main.aba3d00d.js"
+  }
+]);
